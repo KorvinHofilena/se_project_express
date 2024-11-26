86b429b4e694d0825e4bd6468b4292ce77ab0f82
Korvin <hofilenakorvin@gmail.com>
2024-11-26 06:48:50 -0800

    adjusted app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const { connectToDatabase } = require("./db");
-const userRoutes = require("./routes/users");
-const clothingRoutes = require("./routes/clothingItems");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const { ERROR_CODES } = require("./utils/errors");
 
 const { PORT = 3001 } = process.env;
 
@@ -9,25 +9,31 @@ const app = express();
 
 (async () => {
   try {
-    await connectToDatabase();
+    await mongoose.connect("mongodb://127.0.0.1:27017/wtwrdb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to the database");
 
     app.use(express.json());
-
     app.use((req, res, next) => {
       req.user = { _id: "60df3b78c2d7b814c8b5369a" };
       next();
     });
 
-    app.use("/users", userRoutes);
-    app.use("/items", clothingRoutes);
+    app.use(routes);
 
     app.use((req, res) => {
-      res.status(404).send({ message: "Requested resource not found" });
+      res
+        .status(ERROR_CODES.NOT_FOUND)
+        .send({ message: "Requested resource not found" });
     });
 
     app.use((err, req, res, next) => {
-      const { statusCode = 500, message = "An error occurred on the server" } =
-        err;
+      const {
+        statusCode = ERROR_CODES.SERVER_ERROR,
+        message = "An error occurred on the server",
+      } = err;
       res.status(statusCode).send({ message });
     });
 

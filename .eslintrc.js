65c8d719a3bdbaea282a8829fb4c8cd5e30072d4
Korvin <hofilenakorvin@gmail.com>
2024-11-30 17:33:47 -0800

    adjusted .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,13 @@ module.exports = {
     node: true,
   },
   extends: ["eslint:recommended", "airbnb-base", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
-      },
-    },
-  ],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
   },
   rules: {
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
-    "no-console": "warn",
+    "no-console": "off",
+    "no-unused-vars": ["error", { args: "none" }],
   },
 };

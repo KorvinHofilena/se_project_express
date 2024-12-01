1f8a7551a32de5c4ef97d584a4ffc614ac2a3774
Korvin <hofilenakorvin@gmail.com>
2024-11-30 17:36:59 -0800

    adjusted app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const app = express();
     console.log("Connected to the database");
 
     app.use(express.json());
+
     app.use((req, res, next) => {
       req.user = { _id: "60df3b78c2d7b814c8b5369a" };
       next();
@@ -26,7 +27,7 @@ const app = express();
         .send({ message: "Requested resource not found" });
     });
 
-    app.use((err, req, res, next) => {
+    app.use((err, req, res) => {
       const {
         statusCode = ERROR_CODES.SERVER_ERROR,
         message = "An error occurred on the server",

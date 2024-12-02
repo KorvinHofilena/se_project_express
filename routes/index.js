5749f30e91378cb60c901ea62487f26a70001efd
Korvin <hofilenakorvin@gmail.com>
2024-12-02 03:08:59 -0800

    adjusted routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const userRoutes = require("./users");
 const clothingRoutes = require("./clothingItems");
+const { createUser, login } = require("../controllers/users");
+const auth = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.use("/users", userRoutes);
-router.use("/items", clothingRoutes);
+router.post("/signup", createUser);
+router.post("/signin", login);
+
+router.use("/users", auth, userRoutes);
+router.use("/items", auth, clothingRoutes);
 
 module.exports = router;

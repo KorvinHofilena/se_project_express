6115088e387d6875ff51e2e57baea612c640a958
Korvin <hofilenakorvin@gmail.com>
2024-12-12 01:35:49 -0800

    adjusted db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const { MongoClient } = require("mongodb");
 const winston = require("winston");
 
-// Configure Winston logger
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.combine(
@@ -11,14 +10,13 @@ const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
 });
 
-const uri =
-  "mongodb+srv://hofilenakorvin:HnGbkzYSjs5kBoJh@cluster0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb://127.0.0.1:27017";
 const client = new MongoClient(uri);
 
 async function connectToDatabase() {
   try {
     await client.connect();
-    logger.info("Connected to MongoDB!");
+    logger.info("Connected to MongoDB locally!");
     return client;
   } catch (error) {
     logger.error("Error connecting to MongoDB:", error);

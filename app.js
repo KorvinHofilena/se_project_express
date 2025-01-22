8144e655106d99d544cab7cce74b8655b0dd412d
Korvin <hofilenakorvin@gmail.com>
2025-01-22 05:12:45 -0800

    adjusted app.js to avoid async/await

--- app.js
+++ app.js
@@ -19,35 +19,27 @@ const logger = winston.createLogger({
 
 const app = express();
 
-(async () => {
-  try {
-    await connectToDatabase();
-    logger.info("Connected to the database");
+connectToDatabase();
 
-    app.use(express.json());
-    app.use(cors());
-    app.use(routes);
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
-    app.use((req, res) => {
-      res
-        .status(ERROR_CODES.NOT_FOUND)
-        .send({ message: "Requested resource not found" });
-    });
+app.use((req, res) => {
+  res
+    .status(ERROR_CODES.NOT_FOUND)
+    .send({ message: "Requested resource not found" });
+});
 
-    app.use((err, req, res, next) => {
-      const {
-        statusCode = ERROR_CODES.SERVER_ERROR,
-        message = "An error occurred on the server",
-      } = err;
-      logger.error(`Error: ${message}, Status Code: ${statusCode}`);
-      res.status(statusCode).send({ message });
-    });
+app.use((err, req, res, next) => {
+  const {
+    statusCode = ERROR_CODES.SERVER_ERROR,
+    message = "An error occurred on the server",
+  } = err;
+  logger.error(`Error: ${message}, Status Code: ${statusCode}`);
+  res.status(statusCode).send({ message });
+});
 
-    app.listen(PORT, () => {
-      logger.info(`App is running on port ${PORT}`);
-    });
-  } catch (error) {
-    logger.error("Failed to start server:", error);
-    process.exit(1);
-  }
-})();
+app.listen(PORT, () => {
+  logger.info(`App is running on port ${PORT}`);
+});

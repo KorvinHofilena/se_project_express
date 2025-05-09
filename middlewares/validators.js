577d5295fa2be5fbaf0718338940c06e6e56addc
Korvin <hofilenakorvin@gmail.com>
2025-05-09 02:55:47 -0700

    added logger.js and validators.js to middlewares

---
+++ middlewares/validators.js
@@ -0,0 +1,22 @@
+const { celebrate, Joi } = require("celebrate");
+
+const validateSignup = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    avatar: Joi.string().uri(),
+  }),
+});
+
+const validateSignin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  validateSignup,
+  validateSignin,
+};

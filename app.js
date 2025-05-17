f4b68dbc10a9b86aacaa63dedbb46f0e3cd3e07a
Korvin <hofilenakorvin@gmail.com>
2025-05-17 01:05:21 -0700

    adjusted app.js

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const winston = require("winston");
-const morgan = require("morgan");
 const { errors } = require("celebrate");
 
 const { connectToDatabase } = require("./db");
 const routes = require("./routes");
 const { NotFoundError, ERROR_CODES } = require("./utils/errors");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const { PORT = 3001 } = process.env;
-
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json()
-  ),
-  transports: [new winston.transports.Console()],
-});
 
 const app = express();
 
@@ -33,15 +23,11 @@ app.get("/crash-test", () => {
 app.use(express.json());
 app.use(cors());
 
-app.use(
-  morgan("combined", {
-    stream: {
-      write: (message) => logger.info(message.trim()),
-    },
-  })
-);
+app.use(requestLogger);
 
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 
@@ -54,11 +40,9 @@ app.use((err, req, res, _next) => {
     statusCode = ERROR_CODES.SERVER_ERROR,
     message = "An error occurred on the server",
   } = err;
-
-  logger.error(`Error: ${message}, Status Code: ${statusCode}`);
   res.status(statusCode).send({ message });
 });
 
 app.listen(PORT, () => {
-  logger.info(`App is running on port ${PORT}`);
+  console.log(`App is running on port ${PORT}`);
 });
